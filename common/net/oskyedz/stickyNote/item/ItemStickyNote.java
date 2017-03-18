30b238e0bd2c1421ea3b9b6fbfe2ab0a554042e7
Oskar Edholm <minecraft@snakehult.se>
2017-03-18 17:53:05 +0100

    Item + localization
    Wrapped up adding a item and commented everything.

--- common/net/oskyedz/stickyNote/item/ItemStickyNote.java
+++ common/net/oskyedz/stickyNote/item/ItemStickyNote.java
@@ -1,6 +1,5 @@
 package net.oskyedz.stickyNote.item;
 
-import scala.swing.TextComponent;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -11,21 +10,36 @@ import net.minecraft.world.World;
 import net.oskyedz.stickyNote.stickyNote;
 import net.oskyedz.stickyNote.lib.Names;
 
+/**
+ * A very basic item with no subtypes/variants
+ */
+
 public class ItemStickyNote extends Item {
+	
+	  /**
+	   * Called when the item is right-clicked. By overriding this method, we can changed the behavior of the item! Note
+	   * the @Override annotation. You should use these EVERY TIME you override a method. If the method signatures change
+	   * between Minecraft versions (and that happens fairly often), you will get a compiler error. This makes porting much,
+	   * much easier! Otherwise, you'll have a hard time figuring out why certain methods suddenly aren't being called.
+	   */
 
 	@Override 
 	
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
 		
+	if (!worldIn.isRemote) //send message from server 
 		playerIn.sendMessage(new TextComponentString("You used my item!"));
 		return super.onItemRightClick(worldIn, playerIn, handIn);
 		
 	}
 	
+	  /**
+	   * Returns the unlocalized the name for the item. Make sure to add a corresponding line to your localization file!
+	   */
 	@Override
 	public String getUnlocalizedName(ItemStack stack) {
 		
-		return "item." + stickyNote.RESOURCEPREFIX + Names.STICKY_NOTE; // item.stickyNote:sticky_note
+		return "item." + stickyNote.RESOURCEPREFIX + Names.STICKY_NOTE; // item.stickyNote:stickynote_item
 		
 	}
 	
--- common/net/oskyedz/stickyNote/lib/Names.java
+++ common/net/oskyedz/stickyNote/lib/Names.java
@@ -1,11 +1,16 @@
 package net.oskyedz.stickyNote.lib;
 
+/**
+ * Used to store String constants for names of things, like blocks and items. Don't hardcode the names. That opens the possibility of
+ * making a mistake in one place... And if your names don't line up, things will break!
+ */
+
 public class Names {
 	
 /*
  * Items
  */
 
-	public static final String STICKY_NOTE = "sticky_note"; //Only for demonstration purposes. Has to be lower case, can have e underscore. 
+	public static final String STICKY_NOTE = "stickynote_item"; //Only for demonstration purposes. Has to be lower case, can have e underscore. 
 	
 }
